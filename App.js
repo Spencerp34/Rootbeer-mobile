133b17ec9d239d0c7e2ebedc6b2d538d65621f86
Spencer Patterson <spencerpatterson@Spencers-Mac-mini.local>
2023-02-16 14:00:57 -0700

    Adding swiping view

--- App.js
+++ App.js
@@ -2,12 +2,16 @@ import { StyleSheet, Text, SafeAreaView } from 'react-native';
 import { Theme } from './src/constants';
 import Header from './src/components/header';
 import Submission from './src/components/Submission';
+import Swiper from "react-native-swiper";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Header />
+      <Swiper>
+      <Text> test</Text>
       <Submission />
+      </Swiper>
     </SafeAreaView>
   );
 }
